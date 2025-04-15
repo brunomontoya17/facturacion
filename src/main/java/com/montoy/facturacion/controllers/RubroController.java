2a54f2445dbe350f06953d6715a68c19066473d3
brunomontoya17 <brucemontosh17@gmail.com>
2025-04-14 21:01:50 -0300

    Commit Backend

--- src/main/java/com/montoy/facturacion/controllers/RubroController.java
+++ src/main/java/com/montoy/facturacion/controllers/RubroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","http://localhost:4173"})
 @Controller
 @RestController
 @RequestMapping("/rubros")
